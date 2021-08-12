8a0e5c8e3e5607d8e818bc00e401b4a3892a2b5c
JamesSawyer <james3business@gmail.com>
2021-08-12 09:45:09 -0500

    MainComponent created and imported into app.js

---
+++ components/MainComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Main extends Component {
+    
+    render() {
+        return(
+            <View>
+                <Text>Just Testing</Text>
+            </View>
+        );
+    }
+}
+
+export default Main;
